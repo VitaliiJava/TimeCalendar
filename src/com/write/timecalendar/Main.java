ea4eb226348660af48c063765bbe88bf8eb105ed
VitaliiJava <superpills@mail.ru>
2016-09-05 13:48:01 +0300

    commit

---
+++ src/com/write/timecalendar/Main.java
@@ -0,0 +1,12 @@
+package com.write.timecalendar;
+
+/**
+ * Created by Vitalik on 05.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        System.out.println("WWW");
+        System.out.println("My");
+    }
+}
